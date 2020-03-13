5bb0096d18925cd0b4bbc30c1ba74c15b9dd03ea
Sammy Hajhamid <pokeso123@hotmail.com>
2020-03-12 21:54:15 -0700

    init commit

---
+++ code.py
@@ -0,0 +1,57 @@
+from PyDictionary import PyDictionary
+import re
+
+alphabets= "([A-Za-z])"
+prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+websites = "[.](com|net|org|io|gov)"
+
+def split_into_sentences(text):
+    text = " " + text + "  "
+    text = text.replace("\n"," ")
+    text = re.sub(prefixes,"\\1<prd>",text)
+    text = re.sub(websites,"<prd>\\1",text)
+    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+    text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
+    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
+    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
+    text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
+    if "”" in text: text = text.replace(".”","”.")
+    if "\"" in text: text = text.replace(".\"","\".")
+    if "!" in text: text = text.replace("!\"","\"!")
+    if "?" in text: text = text.replace("?\"","\"?")
+    text = text.replace(".",".<stop>")
+    text = text.replace("?","?<stop>")
+    text = text.replace("!","!<stop>")
+    text = text.replace("<prd>",".")
+    sentences = text.split("<stop>")
+    sentences = sentences[:-1]
+    sentences = [s.strip() for s in sentences]
+    return sentences
+
+dictionary = PyDictionary()
+story = open("fahren").read()
+pages = re.split("[\n][\n]\d+[\n][\n]", story)
+sentences = [split_into_sentences(page) for page in pages]
+words = open("words").read().split("\n")
+for word in words:
+    sentence = ""
+    page = 0
+    for i in range(len(sentences)):
+        for i_sentence in sentences[i]:
+            if word in i_sentence:
+                sentence = i_sentence
+                page = i + 1
+    definition = dictionary.meaning(word)
+    word_type = ""
+    for i in definition.keys():
+        word_type = i
+        break
+    if sentence != "":
+        print(f"{word} - {word_type}\n{definition[word_type][0]}.\n\"{sentence}.\"\n Fahrenheit 451, Ray Bradbury pg. {page}")
+
